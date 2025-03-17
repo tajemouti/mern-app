5e3a7f156456685fbf822d800338cd6c6e464464
tajemouti <tajemouti@gmail.com>
2025-03-17 05:32:57 +0000

    Enforce e-mail validation for User schema

--- backend/models/User.js
+++ backend/models/User.js
@@ -4,7 +4,12 @@ const UserSchema = new mongoose.Schema({
   name: { String, required: true },
   age: { Number, required: true },
   role: { String, required: true },
-  email: { String, required: true },
+  email: {
+    String,
+    required: true,
+    unique: true,
+    watch: [/^\S+@\S+\.\S+$/, 'Please enter a valid email address'],
+  },
 });
 
 const User = mongoose.model('User', UserSchema);
