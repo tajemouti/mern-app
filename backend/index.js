a8a0354d64e06feb32095b310e2c1990f2b7f23d
tajemouti <tajemouti@gmail.com>
2025-03-17 20:06:57 +0000

    Add GET /users endpoint to fetch all users

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const express = require('express');
+const User = require('./models/User');
 
 const mongoURI = 'mongodb://localhost:27017/mernDB';
 
@@ -11,3 +12,13 @@ mongoose
 const app = express();
 
 app.use(express.json());
+
+// Get users
+app.get('/users', async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
