edd503e52531f5d4683a5f45892d350c1f57c41a
tajemouti <tajemouti@gmail.com>
2025-03-17 20:04:42 +0000

    Initialize Express server and add JSON body parser middleware

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const express = require('express');
 
 const mongoURI = 'mongodb://localhost:27017/mernDB';
 
@@ -6,3 +7,7 @@ mongoose
   .connect(mongoURI)
   .then(() => console.log('MongoDB connected successfully'))
   .catch((err) => console.error('MongoDB connection error:', err));
+
+const app = express();
+
+app.use(express.json());
