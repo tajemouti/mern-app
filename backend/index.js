59e1a2f48ae2de67f915ffa85d633365a5a2d52c
tajemouti <tajemouti@gmail.com>
2025-03-17 19:34:29 +0000

    Set up MongoDB connection in backend/index.js

---
+++ backend/index.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+const mongoURI = 'mongodb://localhost:27017/mernDB';
+
+mongoose
+  .connect(mongoURI)
+  .then(() => console.log('MongoDB connected successfully'))
+  .catch((err) => console.error('MongoDB connection error:', err));
