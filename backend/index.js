4abd07e2adff9b2d415da9726c5e8ba8581ab09d
tajemouti <tajemouti@gmail.com>
2025-03-19 18:07:42 +0000

    Enable CORS to allow cross-origin requests

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const express = require('express');
+const cors = require('cors');
 const User = require('./models/User');
 
 const mongoURI = 'mongodb://localhost:27017/mernDB';
@@ -7,6 +8,7 @@ const mongoURI = 'mongodb://localhost:27017/mernDB';
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 mongoose
   .connect(mongoURI)
