c96637d572ae7f827093e19a9a26f2b9ef8e5051
tajemouti <tajemouti@gmail.com>
2025-03-19 18:06:42 +0000

    Fetch and display users in frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,46 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const url = 'http://localhost:3000/users';
+
+  const [users, setUsers] = useState([]);
+
+  const fetchUsers = async () => {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setUsers(data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
   return (
     <>
-      <h1>Hello, Mern App</h1>
+      <h1>Uers List</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user._id}>
+            <strong>{user.name}</strong>
+            {' '}
+            <span>
+              {user.role}
+              {' '}
+              (
+              {user.age}
+              {' years old'}
+              )
+              {' '}
+              {user.email}
+            </span>
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
