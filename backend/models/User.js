a216928162207c90e6582568ab59ebaf563caa58
tajemouti <tajemouti@gmail.com>
2025-03-22 23:54:14 +0000

    Hash user password before saving to the database

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
   name: { type: String, required: true },
@@ -11,6 +12,18 @@ const UserSchema = new mongoose.Schema({
     watch: [/^\S+@\S+\.\S+$/, 'Please enter a valid email address'],
   },
   password: { type: String, required: true },
+});
+
+UserSchema.pre('save', async function hashPassword(next) {
+  if (!this.isModified('password')) return next();
+
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    return next();
+  } catch (error) {
+    return next(error);
+  }
 });
 
 const User = mongoose.model('User', UserSchema);
