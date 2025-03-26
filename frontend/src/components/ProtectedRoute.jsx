7267a8cd0f8ef130c0fa33259bd5d78c7625bb6a
tajemouti <tajemouti@gmail.com>
2025-03-26 23:42:12 +0000

    Add ProtectedRoute component to restrict access to authenticated users

---
+++ frontend/src/components/ProtectedRoute.jsx
@@ -0,0 +1,10 @@
+import { Navigate, Outlet } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
+function ProtectedRoute() {
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+
+  return isAuthenticated ? <Outlet /> : <Navigate to="/signin" replace />;
+}
+
+export default ProtectedRoute;
