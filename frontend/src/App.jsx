94903fdeb68e54045762cecc285482fecb10b7a4
tajemouti <tajemouti@gmail.com>
2025-03-26 23:50:40 +0000

    Set up authentication and protected routes in frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,176 +1,20 @@
-import { useEffect, useState } from 'react';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ProtectedRoute from './components/ProtectedRoute';
+import Signup from './components/Signup';
+import Signin from './components/Signin';
+import Dashboard from './components/Dashboard';
 
 function App() {
-  const url = 'http://localhost:3000/users';
-
-  const [users, setUsers] = useState([]);
-  const [name, setName] = useState('');
-  const [age, setAge] = useState();
-  const [role, setRole] = useState('');
-  const [email, setEmail] = useState('');
-  const [editedUser, setEditedUser] = useState(null);
-
-  const fetchUsers = async () => {
-    try {
-      const response = await fetch(url);
-      const data = await response.json();
-      setUsers(data);
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    }
-  };
-
-  useEffect(() => {
-    fetchUsers();
-  }, []);
-
-  const createUser = async () => {
-    const userData = {
-      name, age: Number(age), role, email,
-    };
-
-    try {
-      await fetch(url, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(userData),
-      });
-      fetchUsers();
-    } catch (error) {
-      console.error('Error adding user:', error);
-    }
-  };
-
-  const updateUser = async () => {
-    const userData = {
-      name, age: Number(age), role, email,
-    };
-    try {
-      await fetch(`${url}/${editedUser._id}`, {
-        method: 'PUT',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(userData),
-      });
-      fetchUsers();
-    } catch (error) {
-      console.error('Error updating user:', error);
-    }
-  };
-
-  const deleteUser = async (id) => {
-    try {
-      await fetch(`${url}/${id}`, {
-        method: 'DELETE',
-      });
-      fetchUsers();
-    } catch (error) {
-      console.error('Error deleting user:', error);
-    }
-  };
-
-  const setInputfields = (user) => {
-    setName(user.name);
-    setAge(user.age);
-    setRole(user.role);
-    setEmail(user.email);
-  };
-
-  const resetInputFields = () => {
-    setName('');
-    setAge('');
-    setRole('');
-    setEmail('');
-  };
-
-  const handleCreate = (e) => {
-    e.preventDefault();
-    createUser();
-    resetInputFields();
-  };
-
-  const handleUpdate = (e) => {
-    e.preventDefault();
-    updateUser();
-    setEditedUser(null);
-    resetInputFields();
-  };
-
-  const handleEdit = (user) => {
-    setEditedUser(user);
-    setInputfields(user);
-  };
-
   return (
-    <>
-      <h1>MERN USER MANAGEMENT</h1>
-      <form onSubmit={editedUser ? handleUpdate : handleCreate}>
-        <input
-          type="text"
-          placeholder="Name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          required
-        />
-        <input
-          type="number"
-          placeholder="Age"
-          value={age}
-          onChange={(e) => setAge(e.target.value)}
-          required
-        />
-        <input
-          type="text"
-          placeholder="Role"
-          value={role}
-          onChange={(e) => setRole(e.target.value)}
-          required
-        />
-        <input
-          type="email"
-          placeholder="E-mail"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-        <button
-          type="submit"
-        >
-          {editedUser ? 'Update User' : 'Add User'}
-        </button>
-      </form>
-      <h2>Uers List</h2>
-      <ul>
-        {users.map((user) => (
-          <li key={user._id}>
-            <strong>{user.name}</strong>
-            {' '}
-            <span>
-              {user.role}
-              {' '}
-              (
-              {user.age}
-              {' years old'}
-              )
-              {' '}
-              {user.email}
-            </span>
-            <button
-              type="button"
-              onClick={() => handleEdit(user)}
-            >
-              Edit
-            </button>
-            <button
-              type="button"
-              onClick={() => deleteUser(user._id)}
-            >
-              Delete
-            </button>
-          </li>
-        ))}
-      </ul>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route element={<ProtectedRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
