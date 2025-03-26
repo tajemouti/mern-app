1d2d6d8e15ac1f8dbedcbb8642c8d5c01b7c8ee1
tajemouti <tajemouti@gmail.com>
2025-03-26 23:04:51 +0000

    Create Navigation bar

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import LogoutButton from './LogoutButton';
+
+function Navbar() {
+  return (
+    <div>
+      <h3>Welcome to Dashboard</h3>
+      <LogoutButton />
+    </div>
+  );
+}
+
+export default Navbar;
