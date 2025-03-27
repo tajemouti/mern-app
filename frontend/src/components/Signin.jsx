01ec0289d9ec0de74a3f327853407a5121976dad
tajemouti <tajemouti@gmail.com>
2025-03-27 22:45:13 +0000

    Add sign up prompt to signin page

--- frontend/src/components/Signin.jsx
+++ frontend/src/components/Signin.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { signIn } from '../redux/authSlice';
 
 function Signin() {
@@ -29,23 +29,30 @@ function Signin() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="email"
-        placeholder="Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        required
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        required
-      />
-      <button type="submit">Sign In</button>
-    </form>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <button type="submit">Sign In</button>
+      </form>
+      <p>
+        Don&apos;t have an account yet? Sign up here
+        {' '}
+        <Link to="/signup">Sign Up</Link>
+      </p>
+    </div>
   );
 }
 
