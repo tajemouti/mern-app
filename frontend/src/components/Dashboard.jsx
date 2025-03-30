82415870c899135f7e6048afa2f32b5dbc04d7cc
tajemouti <tajemouti@gmail.com>
2025-03-30 23:45:04 +0000

    Integrate UserManagement component inside dashboard page

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,10 +1,12 @@
 import Navbar from './Navbar';
+import UserManagement from './UserManagement';
 
 function Dashboard() {
   return (
     <div>
       <Navbar />
       <h1>Dashboard</h1>
+      <UserManagement />
     </div>
   );
 }
