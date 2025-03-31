a1e7ebbf9f86720db7e424dddc41e18f2c66d5fb
tajemouti <tajemouti@gmail.com>
2025-03-31 20:51:35 +0000

    Store user data in Redux

--- frontend/src/redux/authSlice.js
+++ frontend/src/redux/authSlice.js
@@ -3,21 +3,33 @@ import { createSlice } from '@reduxjs/toolkit';
 const initialState = {
   token: localStorage.getItem('token') || null,
   isAuthenticated: !!localStorage.getItem('token'),
+  user: JSON.parse(localStorage.getItem('user')) || null,
 };
 
 const authSlice = createSlice({
   name: 'auth',
   initialState,
   reducers: {
-    signIn: (state, action) => ({
-      ...state,
-      token: action.payload,
-      isAuthenticated: true,
-    }),
-    signOut: () => ({
-      token: null,
-      isAuthenticated: false,
-    }),
+    signIn: (state, action) => {
+      const { token, user } = action.payload;
+      localStorage.setItem('token', token);
+      localStorage.setItem('user', JSON.stringify(user));
+      return {
+        ...state,
+        token,
+        isAuthenticated: true,
+        user,
+      };
+    },
+    signOut: () => {
+      localStorage.removeItem('token');
+      localStorage.removeItem('user');
+      return {
+        token: null,
+        isAuthenticated: false,
+        user: null,
+      };
+    },
   },
 });
 
