d6c17c45ec11fbf0bb59d82f7a846ab27d27ec09
tajemouti <tajemouti@gmail.com>
2025-05-10 21:48:20 +0100

    Make name optional when updating user

--- frontend/src/components/UserManagement.jsx
+++ frontend/src/components/UserManagement.jsx
@@ -179,7 +179,7 @@ const UserManagement = () => {
             placeholder="Name"
             value={name}
             onChange={(e) => setName(e.target.value)}
-            required
+            required={!editedUser}
             className="input"
           />
         </div>
